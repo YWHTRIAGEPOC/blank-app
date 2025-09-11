aa6169fd3f0690d55748774cbcefa041dd039615
YWHTRIAGEPOC <pocrepro-ywh-5abc7080c39fe803@yeswehack.ninja>
2025-09-11 09:14:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+with open("test.html", "r") as f:
+    html_content = f.read()
+
+components.html(html_content, height=600, scrolling=True)
